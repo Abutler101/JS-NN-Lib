4e28ed258434474cc418deb4e35da848b0252dbf
Abutler101 <alex@Alex.home>
2017-06-28 15:47:25 +0100

    Added activation function - only sigmoid atm

--- nn.js
+++ nn.js
@@ -1,8 +1,18 @@
-function NeuralNetwork(noI,noH,noO){
+NeuralNetwork.sigmoid = function(x){
+  var res=1/(1+pow(Math.E, -x));
+  return res;
+}
+
+NeuralNetwork.derSigmoid = function(x){
+  return x*(1-x);
+}
+
+function NeuralNetwork(noI,noH,noO,lr,act){
   this.inputNodes = noI;
   this.hiddenNodes = noH;
   this.outputNodes = noO;
-  
+  this.activationFunc = act;
+  this.lr = lr;
 
 
 }
