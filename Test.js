46b2bc3608bc3af591191f7bbc07173cbe2d27e2
Abutler101 <alex@Alex.home>
2017-06-28 22:30:27 +0100

    implemented a test scenario and started to bug fix

---
+++ Test.js
@@ -0,0 +1,28 @@
+//next term calculator
+var numInputNods = 3;
+var numHiddenNod = 20;
+var numOutputNod = 20;
+var network = new NeuralNetwork(numInputNods,numHiddenNod,numOutputNod,'sig');
+var trainingDataInputs = [
+                          [1,2,3],
+                          [0,1,2],
+                          [0,2,4],
+                          [0,3,6],
+                          [3,4,5]
+                        ]
+
+var trainingDataTargs = [
+//                             Units value
+//                         0,1,2,3,4,5,6,7,8,9]
+//                        [0,0,0,0,0,0,0,0,0,0]
+                          [0,0,0,0,0.9,0,0,0,0,0],//expected out 4
+                          [0,0,0,0.9,0,0,0,0,0,0],//expected out 3
+                          [0,0,0,0,0,0,0.9,0,0,0],//expected out 6
+                          [0,0,0,0,0,0,0,0,0,0.9],//expected out 9
+                          [0,0,0,0,0,0,0.9,0,0,0]//expected out 6
+                        ]
+console.log(network.activationFunc);
+for(var i=0;i<trainingDataInputs.length;i++){
+  network.train(trainingDataInputs[i],trainingDataTargs[i]);
+
+}
