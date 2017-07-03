f33bd86d954228075e4fb5c446fff88768efabc4
Abutler101 <alex@Alex.home>
2017-07-03 15:22:08 +0100

    debugging testcase

--- sketch.js
+++ sketch.js
@@ -12,23 +12,27 @@ var totalCorrect = 0;
 var totalGuesses = 0;
 var statusP ='';
 // DATAFrom: https://pjreddie.com/projects/mnist-in-csv/
-
+console.log('launched');
 function preload() {
   training = loadStrings('data/mnist_train_10000.csv');
   testing = loadStrings('data/mnist_test_1000.csv');
 }
 
 function setup() {
+  console.log('entered setup');
   createCanvas(320, 280);
   nn = new NeuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+  console.log('created netwok');
   var saveButton = createButton('save model');
   saveButton.mousePressed(saveModelJSON);
   function saveModelJSON() {
     saveJSON(nn, 'model.json');
   }
+  console.log('ending setup')
 }
 
 function draw() {
+  console.log('frame:',frameCount);
   background(200);
   var traindata = train();
   var result = test();
@@ -48,18 +52,24 @@ function draw() {
 }
 
 function train() {
+  console.log('training');
   var values = training[trainingIndex].split(',');
   var inputs = [];
   for (var i = 1; i < values.length; i++) {
     inputs[i - 1] = map(Number(values[i]), 0, 255, 0, 0.99) + 0.01;
   }
+  console.log('sanitised input');
   targets = [];
   for (var k = 0; k < output_nodes; k++) {
     targets[k] = 0.01;
   }
+  console.log('sanitised targets');
   var label = Number(values[0]);
   targets[label] = 0.99;
+  console.log('feeding values to net');
   nn.train(inputs, targets);
+  //hangs on the above command
+  console.log('trained on inputs:',inputs);
   trainingIndex++;
   if (trainingIndex == training.length) {
     trainingIndex = 0;
