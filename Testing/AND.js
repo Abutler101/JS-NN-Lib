d0ce93d9516d674270c571d3e2d8d01555470402
Abutler101 <alex@Alex.home>
2017-07-04 11:31:42 +0100

    Cleaning - general

---
+++ Testing/AND.js
@@ -0,0 +1,50 @@
+console.log('OBJECTIVE - learn AND gate');
+var inputs = 2;
+var hidden = 10;
+var outputs = 1;
+var nn = new NeuralNetwork(inputs,hidden,outputs);
+
+var inputCase1 = [1,0.01]
+var targeCase1 = [0.01]
+
+var inputCase2 = [0.01,1]
+var targeCase2 = [0.01]
+
+var inputCase3 = [0.01,0.01]
+var targeCase3 = [0.01]
+
+var inputCase4 = [1,1]
+var targeCase4 = [1]
+
+var inputs = [
+                [1,0.01],
+                [0.01,1],
+                [0.01,0.01],
+                [1,1]
+              ]
+
+var targets = [
+                [0.01],
+                [0.01],
+                [0.01],
+                [1]
+              ]
+
+for(var epoch=1;epoch<=1000000;epoch++){
+  if(epoch % 200000 ===0){
+    console.log('epoch: '+epoch)
+  }
+  for(var Tcase = 0;Tcase<inputs.length;Tcase++){
+      nn.train(inputs[Tcase],targets[Tcase]);
+  }
+}
+
+console.log('Training done')
+temp = nn.query(inputs[0])
+console.log(temp+"                SHOULD BE LOW")
+temp = nn.query(inputs[1])
+console.log(temp+"                SHOULD BE LOW")
+temp = nn.query(inputs[2])
+console.log(temp+"                SHOULD BE LOW")
+temp = nn.query(inputs[3])
+console.log(temp+"                SHOULD BE HIGH")
