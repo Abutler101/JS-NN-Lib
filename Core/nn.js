06b81cb538946145ab501c4f10870d013f6e6ba8
Abutler101 <alex@Alex.home>
2017-07-06 13:36:25 +0100

    Added - finished feed forward

--- Core/nn.js
+++ Core/nn.js
@@ -16,14 +16,38 @@ function NeuralNetwork(nodeDistribution,learningRate){
   for(var i=0;i<nodeDistribution.length-1;i++){
     this.weights[i] = new Matrix(nodeDistribution[i+1],nodeDistribution[i]);
     this.weights[i].randomise();
-    console.table(this.weights[i].matrix)
+//    console.table(this.weights[i].matrix)
   }
 }
 
 NeuralNetwork.prototype.train = function(inputsArray,targetsArray){
+  var inputs = Matrix.convFromArray(inputsArray);
+  var targets = Matrix.convFromArray(targetsArray);
+  var outOfLayers = [];
+  //outOfLayers: [fistHiddenOutput,secondHiddenOutput,...,lastHiddenOutput,OutputOfOutput]
+//Start Feed forward
+  for(var i=0;i<this.weights.length;i++){
+    if(i===0){var outOfPrev = inputs;}
+    else{var outOfPrev = outOfLayers[i-1];}
+    inputToHidden = Matrix.dot(this.weights[i],outOfPrev)
+    outOfLayers[i] = Matrix.map(inputToHidden,NeuralNetwork.sigmoid);
+  }
+//End Feed forward
 
+//Start BackProp
+  var errorsOnOutput
+  debugger;
 
 }
+
+
+
+
+
+
+
+
+
 
 NeuralNetwork.prototype.query = function(inputsArray){
 
