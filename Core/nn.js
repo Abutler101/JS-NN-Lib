62174f2ca24c416b52451fa40c7f878a21f38119
Abutler101 <alex@Alex.home>
2017-07-08 07:09:56 +0100

    Added - gradient descent

--- Core/nn.js
+++ Core/nn.js
@@ -13,6 +13,7 @@ function NeuralNetwork(nodeDistribution,learningRate){
   this.nodeDistribution = nodeDistribution;
   this.learningRate = learningRate || 0.1;
   this.weights = [];
+  //weights: [inputToFirstHidden,firstToSecondHidden,...,finalHiddenToOutput]
   for(var i=0;i<nodeDistribution.length-1;i++){
     this.weights[i] = new Matrix(nodeDistribution[i+1],nodeDistribution[i]);
     this.weights[i].randomise();
@@ -35,21 +36,65 @@ NeuralNetwork.prototype.train = function(inputsArray,targetsArray){
 //End Feed forward
 
 //Start BackProp
-  var errorsOnOutput
-  debugger;
+  var errors = [];
+  //errors: [0] ->error on output layer
+  //        [1] ->error on last Hidden layer
+  //        ....
+  //        [50] ->error on first Hidden layer
+  errors[0] = Matrix.subtract(targets,outOfLayers[outOfLayers.length-1]);
+  this.weightsReversed = this.weights.reverse();
+  this.transposedWeights = [];
+  for(var i = 0;i<this.weights.length;i++){
+    this.transposedWeights[i] = this.weightsReversed[i].transpose();
+  }
+  for(var i = 1;i<outOfLayers.length;i++){
+    errors[i] = Matrix.dot(this.transposedWeights[i-1],errors[i-1]);
+  }
+//End Backprop
+//Start Gradient descent
+  this.Gradients = []
+  //Gradients:      [0] -> gradient for output
+  //                [1] -> gradient for last hidden layer
+  //                ...
+  //                [5] -> gradient for first hidden layer
+  for(var i =0;i<outOfLayers.length;i++){
+    this.Gradients[i]=Matrix.map(outOfLayers[outOfLayers.length-1],NeuralNetwork.derSigmoid);
+    this.Gradients[i].multiply(errors[i]);
+    this.Gradients[i].multiply(this.learningRate);
+  }
+//End Gradient descent
+
+//Start weight changeing
+  this.transposedOutputs = [];
+  for(var i = 0;i<outOfLayers.length;i++){
+    this.transposedOutputs[i] = outOfLayers[i].transpose();
+  }
+  this.weightChanges = [];
+  //weightChanges: [0] ->change on last hidden to output
+  //               [1] -> change on penultimate hidden to last hidden layer
+  //               ...
+  //               [5] ->change on input to first hidden
+
+  for(var i=0; i<this.transposedOutputs.length;i++){
+    //CALCULATE CHNAGES TO THE WEIGHTS
+  }
+
+  for(var i = 0; i<this.weightsReversed.length;i++){
+    //ADD THE CHANGES TO THE WEIGHTS
+  }
+
 
 }
 
-
-
-
-
-
-
-
-
-
 NeuralNetwork.prototype.query = function(inputsArray){
-
-
+  var inputs = Matrix.convFromArray(inputsArray);
+  var outOfLayers = [];
+  //outOfLayers: [fistHiddenOutput,secondHiddenOutput,...,lastHiddenOutput,OutputOfOutput]
+  for(var i=0;i<this.weights.length;i++){
+    if(i===0){var outOfPrev = inputs;}
+    else{var outOfPrev = outOfLayers[i-1];}
+    inputToHidden = Matrix.dot(this.weights[i],outOfPrev);
+    outOfLayers[i] = Matrix.map(inputToHidden,NeuralNetwork.sigmoid);
+  }
+  return outOfLayers[outOfLayers.length-1].convToArray();
 }
