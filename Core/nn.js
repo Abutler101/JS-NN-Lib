bbb7cf53ff083a2d997214db5cc88e2eb19d808f
Abutler101 <alex@Alex.home>
2017-07-14 09:04:21 +0100

    removed 3 layer cap

--- Core/nn.js
+++ Core/nn.js
@@ -1,5 +1,4 @@
-var targetsglob;
-var outOfOutputglob;
+
 NeuralNetwork.sigmoid = function(x){
   var res=1/(1+Math.pow(Math.E, -x));
   return res;
@@ -9,53 +8,74 @@ NeuralNetwork.derSigmoid = function(x){
   return x*(1-x);
 }
 
-function NeuralNetwork(noI,noH,noO,lr){
-  this.inputNodes = noI;
-  this.hiddenNodes = noH;
-  this.outputNodes = noO;
-  this.learnRa = lr || 0.1;
-// only 3 layers - I->H->O
-  this.weightsInputToHidden = new Matrix(this.hiddenNodes,this.inputNodes);
-  this.weightsHiddenToOutput = new Matrix(this.outputNodes,this.hiddenNodes);
-  this.weightsInputToHidden.randomise();
-  this.weightsHiddenToOutput.randomise();
+function NeuralNetwork(nodeDistribution,learningRate){
+  this.nodeDistribution = nodeDistribution;
+  this.learningRate = learningRate || 0.1;
+  this.weights = [];
+  for(var i=0;i<nodeDistribution.length-1;i++){
+    this.weights[i] = new Matrix(nodeDistribution[i+1],nodeDistribution[i]);
+    this.weights[i].randomise();
+  }
 }
 
 NeuralNetwork.prototype.train = function(inputsArray,targetsArray){
-//feed forward
   var inputs = Matrix.convFromArray(inputsArray);
   var targets = Matrix.convFromArray(targetsArray);
-  var inputToHidden = Matrix.dot(this.weightsInputToHidden,inputs);
-  var outOfHidden = Matrix.map(inputToHidden,NeuralNetwork.sigmoid);
-  var inputToOutput = Matrix.dot(this.weightsHiddenToOutput,outOfHidden);
-  var outOfOutput = Matrix.map(inputToOutput,NeuralNetwork.sigmoid);
-//back prop
-  var errorsOnOutput = Matrix.subtract(targets,outOfOutput);
-  var weightsHiddenToOutputTrans = this.weightsHiddenToOutput.transpose();
-  var errorsOnHidden = Matrix.dot(weightsHiddenToOutputTrans, errorsOnOutput);
-//Gradient slide
-  var gradientOutput = Matrix.map(outOfOutput,NeuralNetwork.derSigmoid);
-  gradientOutput.multiply(errorsOnOutput);
-  gradientOutput.multiply(this.learnRa);
-  var gradientHidden = Matrix.map(outOfHidden,NeuralNetwork.derSigmoid);
-  gradientHidden.multiply(errorsOnHidden);
-  gradientHidden.multiply(this.learnRa);
-//Change weights of hidden to outpput feed
-  var outOfHiddenTrans = outOfHidden.transpose();
-  var changeInHiddenToOutputWeights = Matrix.dot(gradientOutput,outOfHiddenTrans);
-  this.weightsHiddenToOutput.add(changeInHiddenToOutputWeights);
-//Change weights of input to hidden feed
-  var inputsTrans = inputs.transpose();
-  var changeInInputToHiddenWeights = Matrix.dot(gradientHidden,inputsTrans);
-  this.weightsInputToHidden.add(changeInInputToHiddenWeights);
+  var outOfLayers = [];
+//Start Feed forward
+  for(var i=0;i<this.weights.length;i++){
+    if(i===0){var outOfPrev = inputs;}
+    else{var outOfPrev = outOfLayers[i-1];}
+    inputToHidden = Matrix.dot(this.weights[i],outOfPrev)
+    outOfLayers[i] = Matrix.map(inputToHidden,NeuralNetwork.sigmoid);
+  }
+//End Feed forward
+//Start BackProp
+  var errors = [];
+  errors[0] = Matrix.subtract(targets,outOfLayers[outOfLayers.length-1]);
+  this.weightsReversed = this.weights.reverse();
+  this.transposedWeights = [];
+  for(var i = 0;i<this.weights.length;i++){
+    this.transposedWeights[i] = this.weightsReversed[i].transpose();
+  }
+  for(var i = 1;i<outOfLayers.length;i++){
+    errors[i] = Matrix.dot(this.transposedWeights[i-1],errors[i-1]);
+  }
+//End Backprop
+//Start Gradient descent
+  this.Gradients = []
+  for(var i =0;i<outOfLayers.length;i++){
+    this.Gradients[i]=Matrix.map(outOfLayers[outOfLayers.length-1-i],NeuralNetwork.derSigmoid);
+    this.Gradients[i].multiply(errors[i]);
+    this.Gradients[i].multiply(this.learningRate);
+  }
+//End Gradient descent
+//Start weight changeing
+  this.weightChanges = [];
+  for(var i=0;i<this.weights.length;i++){
+    if(i!=this.weights.length-1){
+      this.weightChanges[i] = Matrix.dot(this.Gradients[i],(outOfLayers[outOfLayers.length-2-i].transpose()));
+    }
+    else{
+      this.weightChanges[i] = Matrix.dot(this.Gradients[i],(inputs.transpose()));
+    }
+  }
+
+  for(var i = 0; i<this.weightsReversed.length;i++){
+    this.weightsReversed[i].add(this.weightChanges[i]);
+  }
+  this.weights = this.weightsReversed.reverse();
+//End weight changeing
 }
 
-//How to feed data to get actual results
 NeuralNetwork.prototype.query = function(inputsArray){
   var inputs = Matrix.convFromArray(inputsArray);
-  var inputToHidden = Matrix.dot(this.weightsInputToHidden,inputs);
-  var outOfHidden = Matrix.map(inputToHidden,NeuralNetwork.sigmoid);
-  var inputToOutput = Matrix.dot(this.weightsHiddenToOutput,outOfHidden);
-  var outOfOutput = Matrix.map(inputToOutput,NeuralNetwork.sigmoid);
-  return outOfOutput.convToArray();
+  var outOfLayers = [];
+  for(var i=0;i<this.weights.length;i++){
+    if(i===0){var outOfPrev = inputs;}
+    else{var outOfPrev = outOfLayers[i-1];}
+    inputToHidden = Matrix.dot(this.weights[i],outOfPrev);
+    outOfLayers[i] = Matrix.map(inputToHidden,NeuralNetwork.sigmoid);
+  }
+  return outOfLayers[outOfLayers.length-1].convToArray();
 }
